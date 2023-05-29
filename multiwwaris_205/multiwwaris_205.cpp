30449dde05585f0e948be120bcff2b13b86fec8a
zikirazizi <zikir.azizi.ft22@mail.umy.ic.id>
2023-05-29 15:19:50 +0700

    membuat class orang

--- multiwwaris_205/multiwwaris_205.cpp
+++ multiwwaris_205/multiwwaris_205.cpp
@@ -1,4 +1,15 @@
 #include <iostream>
 using namespace std;
 
+class orang {
+public:
+	int umur;
+
+	orang(int pUmur) :
+		umur(pUmur)
+	{
+		cout << "orang dibuat dengan umur " << umur << "\n" << endl;
+	}
+};
+
 
