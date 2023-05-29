01225b318bffd7cd0430e185f07cd4fffdf797bd
zikirazizi <zikir.azizi.ft22@mail.umy.ic.id>
2023-05-29 14:48:58 +0700

    Add project files.

---
+++ multipewarisan.cpp_205/multipewarisan.cpp_205.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class orang {
+public: 
+	string nama;
+
+	orang(string pNama) :
+		nama(pNama) {
+		cout << "orang dibuat\n" << endl;
+	}
+	~orang() {
+		cout << "orang dihapus\n" << endl;
+	}
+
+};
