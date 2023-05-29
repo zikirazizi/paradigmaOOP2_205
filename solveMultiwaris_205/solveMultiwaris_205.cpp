4dd886822f7d0d3045c0535076aba758514e9b4c
zikirazizi <zikir.azizi.ft22@mail.umy.ic.id>
2023-05-29 15:45:17 +0700

    membuat class budi

--- solveMultiwaris_205/solveMultiwaris_205.cpp
+++ solveMultiwaris_205/solveMultiwaris_205.cpp
@@ -22,3 +22,25 @@ public:
 	}
 
 };
+
+class pelajar : virtual public orang {
+public:
+
+	pelajar(int pUmur) :
+		orang(pUmur)
+	{
+		cout << "pelajar dibuat\n" << endl;
+	}
+};
+
+class budi : public pekerja, public pelajar {
+public:
+
+	budi(int pUmur) :
+		pekerja(pUmur),
+		pelajar(pUmur),
+		orang(pUmur) //hal ini dapat dilakukan jika menggunakan virtual
+	{
+		cout << "budi dibuat\n" << endl;
+	}
+};
