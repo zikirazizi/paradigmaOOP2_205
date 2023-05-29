163b9470ffc7544332d893b9e47b231b1b203c61
zikirazizi <zikir.azizi.ft22@mail.umy.ic.id>
2023-05-29 15:03:11 +0700

    membuat class pelajar

--- multipewarisan.cpp_205/multipewarisan.cpp_205.cpp
+++ multipewarisan.cpp_205/multipewarisan.cpp_205.cpp
@@ -19,4 +19,22 @@ public:
 class manusia {
 public:
 	string jeniskelamin;
+
+	manusia(string pJeniskelamin) :
+		jeniskelamin(pJeniskelamin) {
+		cout << "manusia dibuat\n" << endl;
+	}
 };
+
+class pelajar : private manusia, public orang {
+public: 
+	string sekolah;
+
+	pelajar(string pNama, string pJeniskelamin, string pSekolah) :
+		orang(pNama),
+		manusia(pJeniskelamin),
+		sekolah(pSekolah) {
+		cout << "pelajar dibuat\n" << endl;
+	}
+};
+
