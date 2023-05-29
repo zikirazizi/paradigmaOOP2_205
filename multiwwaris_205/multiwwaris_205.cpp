1842ca663caee9763df77b282c7979d59cc18c8a
zikirazizi <zikir.azizi.ft22@mail.umy.ic.id>
2023-05-29 15:27:37 +0700

    membuat class budi

--- multiwwaris_205/multiwwaris_205.cpp
+++ multiwwaris_205/multiwwaris_205.cpp
@@ -32,4 +32,15 @@ public:
 	}
 };
 
+class budi : public pekerja, public pelajar {
+public:
+
+	budi(int pUmur) :
+		pekerja(pUmur),
+		pelajar(pUmur)
+	{
+		cout << "budi dibuat\n" << endl;
+	}
+};
+
 
